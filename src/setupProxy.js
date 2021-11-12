6aa304e02a46ed96f728bb97be22386cbe3cabce
ChangjunPark <ckdwns9121@naver.com>
2021-11-12 16:55:16 +0900

    # API PATH

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
-const {API_PATH} = require('./core/utils/path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const URL = 'http://13.125.94.243:4000';
 
 
 module.exports = function(app){
   app.use(
       createProxyMiddleware('/api', {
-          target: API_PATH,
+          target: URL,
           changeOrigin: true
       })
   )
